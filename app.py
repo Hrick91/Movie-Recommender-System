cdba74073fc014668272db50affde539ad4042a0
Arpan Paul <145971102+Hrick91@users.noreply.github.com>
2025-01-03 00:21:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def recommend(movie):
 
 st.header('Movie Recommender System')
 movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+similarity = pickle.load(open('model/sim_5000.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
